696a58f1e64f0b0caf71557e555921bd56825145
Romell Walker <walkerromell@gmail.com>
2025-09-23 16:41:48 -0400

    Update Shopping list

---
+++ src/main/java/com/pluralsight/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("What's Up World ");
+
+    }
+}
---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Apple, Grapes, Cheese, Bread, Mayo, Orange juice, Chicken, Burgers, Buns, Rum");
+    }
+}
